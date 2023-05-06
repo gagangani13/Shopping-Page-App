033cd762157b3e40d9dfc33360777e91c3d97c74
gagangani13 <gagangani17@gmail.com>
2023-05-06 13:40:47 +0530

    Fixing the Cart problems Action Thunk is used

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import store from './components/Store/store';
-import { Provider } from 'react-redux';
+import {useSelector,useDispatch} from 'react-redux'
+import Notification from './components/UI/Notification';
+import { loadCartThunk, sendCartDataThunk } from './components/Store/cartSlice';
+let initialRun=true;
 function App() {
+  const cart=useSelector((state)=>state.cartReducer.items)
+  const notify=useSelector((state)=>state.cartReducer.notification)
+  const dispatch=useDispatch()
+  useEffect(()=>{
+      if (initialRun) {
+        initialRun=false
+        dispatch(loadCartThunk())
+        return;
+      }
+      dispatch(sendCartDataThunk(cart))
+  },[cart,dispatch])
+
   return (
-    <Provider store={store}>
+    <>
+    {notify&&<Notification title={notify.title} status={notify.status} message={notify.message} />}
     <Layout>
       <Cart />
       <Products />
     </Layout>
-    </Provider>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-
+import store from './components/Store/store';
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}><App /></Provider>);
