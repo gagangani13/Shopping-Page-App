cd64ca285245d194b5ec3e46c1a6c58198fecddc
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-05-06 13:58:27 +0530

    Fixing the cart problems (#2)
    * Add to Cart Fixes using redux
    
    * Fixing the Cart problems
    Action Thunk is used

--- src/components/Store/cartSlice.js
+++ src/components/Store/cartSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialCart = { cartOpen: false, items: []};
+const initialCart = { cartOpen: false, items: [],notification:null};
 const cartSlice = createSlice({
   name: "Cart",
   initialState: initialCart,
@@ -13,6 +13,13 @@ const cartSlice = createSlice({
     },
     updateItem(state,action){
         state.items=action.payload
+    },
+    setNotification(state,action){
+        state.notification={
+            status:action.payload.status,
+            title:action.payload.title,
+            message:action.payload.message
+        }
     }
 
   },
@@ -20,3 +27,43 @@ const cartSlice = createSlice({
 export const cartAction = cartSlice.actions;
 export const cartReducer = cartSlice.reducer;
 export default cartSlice;
+//this function returns action
+export const sendCartDataThunk=(cart)=>{
+    return async function (dispatch) {
+        dispatch(cartAction.setNotification({status:'pending',message:'Sending cart data',title:'Sending...'}))
+        
+        const sendRequest=async()=>{
+            const response=await fetch(`https://shopping-page-app-4a7ac-default-rtdb.firebaseio.com/Cart.json`,
+            {
+            method:'PUT',
+            body:JSON.stringify(cart)
+        })
+        if(!response.ok){
+            throw new Error()
+          }  
+        }
+        try {
+             await sendRequest()
+             dispatch(cartAction.setNotification({status:'success',message:'Sent cart data successfully!!',title:'Success'}))
+        } catch (error) {
+            dispatch(cartAction.setNotification({status:'error',message:'Sending cart data failed!!',title:'Error'}))
+        }
+      
+    }
+}
+export const loadCartThunk=()=>{
+    return async function (dispatch) {
+      const response=await fetch(`https://shopping-page-app-4a7ac-default-rtdb.firebaseio.com/Cart.json`)
+      const data= await response.json()
+      try {
+        if(response.ok){
+          dispatch(cartAction.updateItem(data))
+        }else{
+          throw new Error()
+        }
+      } catch (error) {
+        console.log('No data')
+      }
+    }
+}
+
